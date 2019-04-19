8f1122bb7a3c87256fe2d3d853173a9d520a24f4
tomoki1207 <tomoki1207@gmail.com>
2019-04-19 12:59:33 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: {
+    options: './src/options.js',
+    content_script: './src/content_script.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '.build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.jsx$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: { presets: ['@babel/env'] }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  plugins: [new CopyWebpackPlugin([{ from: 'src/static', to: '.' }])],
+  devtool:
+    process.env.NODE_ENV === 'production' ? false : 'cheap-module-source-map'
+}

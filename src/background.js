2aca1e74949ac6ea4d1373fedfa507aa465610fe
tomoki1207 <tomoki1207@gmail.com>
2019-04-22 17:02:15 +0900

    apply generated icon to channel

--- src/background.js
+++ src/background.js
@@ -1,16 +1,45 @@
-// request by serial with interval to avoid API limit.
-const delay = async x =>
+import { createHash } from 'crypto'
+import Identicon from 'identicon.js'
+
+const insertedCss = {}
+
+const generateDefaultIconSrc = channel => {
+  const sha = createHash('sha1')
+  sha.update(channel.name)
+  const hash = sha.digest('hex')
+  const iconData = new Identicon(hash, { size: 20, margin: 0.1, format: 'svg' })
+  return `data:image/svg+xml;base64,${iconData.toString()}`
+}
+
+const replaceIconCss = channel => {
+  const id = channel.id
+  if (insertedCss[id]) {
+    browser.tabs.removeCSS({ code: prev })
+  }
+  const css = `
+  div#col_channels a.c-link.p-channel_sidebar__channel[href$="${id}"] > span:before {
+    background: url(${generateDefaultIconSrc(channel)}) no-repeat center center;
+    padding: 10px;
+    margin-right: 5px;
+    content: ""
+  }`
+  browser.tabs.insertCSS({ code: css })
+  insertedCss[id] = css
+}
+
+const trivialSleep = ms =>
   new Promise(resolve => {
     setTimeout(() => {
-      resolve(x)
-    }, 1500)
+      resolve()
+    }, ms)
   })
 
+// request by serial with interval to avoid API limit.
 const serial = async promises => {
   for (const p of promises) {
     try {
       await p()
-      await delay()
+      await trivialSleep(1500)
     } catch (e) {
       // noop
       console.log(e)
@@ -37,7 +66,7 @@ const fetchChannel = async (channel, origin, token) => {
   const url = `${origin}/api/channels.info`
   const response = await requestAsFormData(url, param)
   const body = await response.json()
-  // console.log(body.channel)
+  replaceIconCss(body.channel)
 }
 
 const fetchGroup = async (group, origin, token) => {
@@ -48,7 +77,7 @@ const fetchGroup = async (group, origin, token) => {
   const url = `${origin}/api/groups.info`
   const response = await requestAsFormData(url, param)
   const body = await response.json()
-  // console.log(body.group)
+  replaceIconCss(body.group)
 }
 
 const listener = detail => {
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
-  target: 'node',
+  target: 'web',
   entry: {
     options: './src/options.js',
     content_script: './src/content_script.js',

7dcbde7b9d5766a34f3687ffa449ef99d19e9f61
tomoki1207 <tomoki1207@gmail.com>
2019-04-22 12:41:44 +0900

    intercept channels request
    to obtain channels/groups detail by original request info.

--- src/background.js
+++ src/background.js
@@ -1,23 +1,93 @@
+// request by serial with interval to avoid API limit.
+const delay = async x =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x)
+    }, 1500)
+  })
+
+const serial = async promises => {
+  for (const p of promises) {
+    try {
+      await p()
+      await delay()
+    } catch (e) {
+      // noop
+      console.log(e)
+    }
+  }
+  return Promise.resolve()
+}
+
+const requestAsFormData = async (url, params) => {
+  const searchParams = new URLSearchParams()
+  for (const key in params) {
+    if (params.hasOwnProperty(key)) {
+      searchParams.append(key, params[key])
+    }
+  }
+  return fetch(`${url}?${searchParams.toString()}`)
+}
+
+const fetchChannel = async (channel, origin, token) => {
+  const param = {
+    token,
+    channel: channel.id
+  }
+  const url = `${origin}/api/channels.info`
+  const response = await requestAsFormData(url, param)
+  const body = await response.json()
+  // console.log(body.channel)
+}
+
+const fetchGroup = async (group, origin, token) => {
+  const param = {
+    token,
+    channel: group.id
+  }
+  const url = `${origin}/api/groups.info`
+  const response = await requestAsFormData(url, param)
+  const body = await response.json()
+  // console.log(body.group)
+}
+
 const listener = detail => {
   const filter = browser.webRequest.filterResponseData(detail.requestId)
   const decoder = new TextDecoder('utf-8')
+  const requestUrl = new URL(detail.url)
+  const token = detail.requestBody.formData.token
 
+  let buffer = ''
   filter.ondata = event => {
     const payload = decoder.decode(event.data, { stream: true })
-    console.log(payload)
+    // return response as is.
     filter.write(event.data)
-    filter.disconnect()
+    buffer += payload
   }
 
   filter.onstop = event => {
-    filter.close()
+    filter.disconnect()
+    const body = JSON.parse(buffer)
+    Promise.all([
+      serial(
+        body.channels.map(channel => async () =>
+          fetchChannel(channel, requestUrl.origin, token)
+        )
+      ),
+      serial(
+        body.groups.map(group => async () =>
+          fetchGroup(group, requestUrl.origin, token)
+        )
+      )
+    ])
   }
 }
 
+// peek request contains channels list
 browser.webRequest.onBeforeRequest.addListener(
   listener,
   {
-    urls: ['https://*.slack.com/*']
+    urls: ['https://*.slack.com/api/users.counts*']
   },
-  ['blocking']
+  ['blocking', 'requestBody']
 )
